c428ae43b706e008fae18b0af1119aaa0156199c
Vinicius Felipe <viniciusssf3@gmail.com>
2020-10-25 09:44:00 -0300

    criando teste de p-table.

---
+++ src/app/testes-angular/teste-p-table/teste-p-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste-p-table',
+  templateUrl: './teste-p-table.component.html',
+  styleUrls: ['./teste-p-table.component.css']
+})
+export class TestePTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
