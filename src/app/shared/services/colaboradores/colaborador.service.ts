d17625695c8921c14b4c14bb6915f0f030bc6a79
Vinicius Felipe <viniciusssf3@gmail.com>
2021-01-03 00:31:09 -0300

    adicionando novo endpoint.

--- src/app/shared/services/colaboradores/colaborador.service.ts
+++ src/app/shared/services/colaboradores/colaborador.service.ts
@@ -2,6 +2,7 @@ import { Colaborador } from '../../models/Colaborador';
 import { BACKEND_URL } from '../../config/config';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +11,15 @@ export class ColaboradorService {
 
   private serviceUrl = `${BACKEND_URL}/colaborador`;
 
-  /*private _httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      'Access-Control-Allow-Headers':
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-    })
-  }*/
 
   constructor(private http: HttpClient) { }
 
   indexar(entidade: Colaborador){
     //return this.http.post<Colaborador>(`${this.serviceUrl}/indexar`,entidade,this._httpOptions)
     return this.http.post<Colaborador>(`${this.serviceUrl}/indexar`,entidade);
+  }
+
+  searchByName(nome: string) : Observable<Colaborador[]>{
+    return this.http.get<Colaborador[]>(`${this.serviceUrl}/{$nome}`);
   }
 }
