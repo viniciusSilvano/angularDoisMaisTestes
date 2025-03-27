e7bb8e479a2fd3997f59775af358e36a0271ac92
Vinicius Felipe <viniciusssf3@gmail.com>
2025-03-26 23:20:13 -0300

    Adding browser's cache test in home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { CacheService } from './../services/cache.service';
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
@@ -9,11 +10,12 @@ import { MenuItem } from 'primeng/api';
 export class HomePageComponent implements OnInit {
 
   items: MenuItem[];
-  constructor() { }
+  chaveCache: string = 'CBB'
+  constructor(private cacheService: CacheService) { }
 
   ngOnInit(): void {
     this.items = [
-      { 
+      {
         label: 'Java Test Project',
         icon: 'pi pi-folder',
         routerLink:"/home-page/java-test-project"
@@ -24,6 +26,38 @@ export class HomePageComponent implements OnInit {
         routerLink:"/home-page/testes-angular"
       }
     ]
+  }
+
+  testeCache(){
+    let map = new Map();
+    localStorage.myMap = JSON.stringify(Array.from(map.entries()));
+    console.log('Inserindo caches');
+    this.cacheService.inserirCache(`${this.chaveCache}_42010_06`,'500');
+    this.cacheService.inserirCache(`${this.chaveCache}_52024_06`,'600');
+
+    console.log('recuperando cache se existir')
+    if(this.cacheService.contemCache(`${this.chaveCache}_42010_06`)){
+      let valor = this.cacheService.recuperarCache(`${this.chaveCache}_42010_06`);
+      console.log('valor de cache recuperado: ', valor);
+    }
+
+    if(this.cacheService.contemCache(`${this.chaveCache}_52024_06`)){
+      let valor = this.cacheService.recuperarCache(`${this.chaveCache}_52024_06`);
+      console.log('valor de cache recuperado: ', valor);
+    }
+
+  }
+
+  removerCache(){
+    this.cacheService.removerCache();
+  }
+
+  AtivarDesativarCache(){
+    this.cacheService.AtivarDesativarCache();
+  }
+
+  cacheEstaAtivo() : boolean{
+    return this.cacheService.cacheEstaAtivo();
   }
 
 }
---
+++ src/app/services/cache.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheService {
+
+  setChaves : Set<string> = new Set<string>();
+  chaveCacheSetChavesApuracoes = "CHAVES_SET_APURACOES";
+
+  cacheAtivo : boolean = true;
+  chaveCacheAtivo = "CHAVE_CACHE_ATIVO_APURACOES"
+
+  constructor() {
+    this.setChaves =
+    localStorage.getItem(this.chaveCacheSetChavesApuracoes)
+    ? new Set(JSON.parse(localStorage.getItem(this.chaveCacheSetChavesApuracoes)))
+    : new Set<string>();
+
+    this.cacheAtivo =
+    localStorage.getItem(this.chaveCacheAtivo) ?
+    (/true/i).test(localStorage.getItem(this.chaveCacheAtivo))
+    : true;
+
+    localStorage.setItem(this.chaveCacheAtivo, this.cacheAtivo.toString());
+  }
+
+  contemCache(chaveCache) : boolean{
+    if(this.cacheAtivo){
+      return this.setChaves.has(chaveCache);
+    }else{
+      return false;
+    }
+  }
+
+  inserirCache(chaveCache: string, valor: string){
+    if(this.cacheAtivo){
+      console.log('set durante inserção: ',  this.setChaves);
+      this.setChaves.add(chaveCache);
+      console.log('set após o add', this.setChaves);
+      console.log('set convertido para array e stringfy: ', JSON.stringify([...this.setChaves]))
+      localStorage.setItem(chaveCache,valor);
+      localStorage.setItem(this.chaveCacheSetChavesApuracoes,JSON.stringify([...this.setChaves]));
+    }
+  }
+
+  recuperarCache(chaveCache: string){
+    return localStorage.getItem(chaveCache);
+  }
+
+  removerCache(){
+    for(let chave of this.setChaves){
+      console.log('chave recuperada do set: ', chave);
+      localStorage.removeItem(chave);
+    }
+    this.setChaves.clear();
+    localStorage.removeItem(this.chaveCacheSetChavesApuracoes);
+  }
+
+  AtivarDesativarCache(){
+    this.cacheAtivo = !this.cacheAtivo;
+    localStorage.setItem(this.chaveCacheAtivo, this.cacheAtivo.toString());
+  }
+
+  cacheEstaAtivo(){
+    return this.cacheAtivo;
+  }
+}
